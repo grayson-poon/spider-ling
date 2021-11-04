99edce403374058d395986892b20a59d4217017c
grayson-poon <grayson.a.poon@gmail.com>
2021-11-04 11:53:37 -0700

    change location of spiderman image

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -13,7 +13,7 @@ class Player {
     this.maxImpulse = 200;
 
     let spidey = new Image();
-    spidey.src = "assets/sprite_spiderman.png";
+    spidey.src = "./assets/sprite_spiderman.png";
     this.image = spidey;
     this.width = 35;
     this.height = 70;
