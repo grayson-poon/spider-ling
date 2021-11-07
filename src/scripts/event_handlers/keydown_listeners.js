267c1a989beb1ab1db090da38a89f5bee26a3c86
grayson-poon <grayson.a.poon@gmail.com>
2021-11-07 01:37:12 -0700

    create separate functions for keydown handlers

--- src/scripts/event_handlers/keydown_listeners.js
+++ src/scripts/event_handlers/keydown_listeners.js
@@ -6,51 +6,64 @@ export default function addKeydownEventListeners(player, level, pauseStatus) {
 
 function addKeydownListener(player, level, pauseStatus) {
   if (pauseStatus === false) {
-    document.addEventListener("keydown", handleKeydown(player, level));
+    document.addEventListener("keydown", handleKeydown(player, level, pauseStatus));
   } else {
-    document.removeEventListener("keydown", handleKeydown(player, level));
+    document.removeEventListener("keydown", handleKeydown(player, level, pauseStatus));
   }
 }
 
-function handleKeydown(player, level) {
+function handleKeydown(player, level, pauseStatus) {
   return function(event) {
+    if (pauseStatus) return;
     event.stopPropagation();
     event.preventDefault();
 
     const key = event.keyCode;
     if (key === 65) {
-      let closestL = wallUtil.closestWallToTheLeft(player, level.arrWalls);
-      let distanceL = wallUtil.distanceToTheLeft(player, closestL);
-
-      if (distanceL === 1) {
-        return
-      } else if (distanceL > player.velocity) {
-        player.left();
-      } else {
-        player.x -= (distanceL - 1);
-      }
+      handleLeftKey(player, level);
     } else if (key === 68) {
-      let closestR = wallUtil.closestWallToTheRight(player, level.arrWalls);
-      let distanceR = wallUtil.distanceToTheRight(player, closestR);
-
-      if (distanceR === 1) {
-        return
-      } else if (distanceR > player.velocity) {
-        player.right();
-      } else {
-        player.x += (distanceR - 1);
-      }
+      handleRightKey(player, level)
     } else if (key === 32) {
-      let closestB = wallUtil.closestWallBelow(player, level.arrWalls);
-      let distanceB = wallUtil.distanceBelow(player, closestB);
-      
-      if (distanceB > 2) {
-        player.jumping = true;
-        return
-      } else {
-        player.jumping = true;
-        player.jump();
-      }
+      handleSpacebarKey(player, level);
     }
+  }
+}
+
+function handleLeftKey(player, level) {
+  let closestL = wallUtil.closestWallToTheLeft(player, level.arrWalls);
+  let distanceL = wallUtil.distanceToTheLeft(player, closestL);
+
+  if (distanceL === 1) {
+    return
+  } else if (distanceL > player.velocity) {
+    player.left();
+  } else {
+    player.x -= (distanceL - 1);
+  }
+}
+
+function handleRightKey(player, level) {
+  let closestR = wallUtil.closestWallToTheRight(player, level.arrWalls);
+  let distanceR = wallUtil.distanceToTheRight(player, closestR);
+
+  if (distanceR === 1) {
+    return
+  } else if (distanceR > player.velocity) {
+    player.right();
+  } else {
+    player.x += (distanceR - 1);
+  }
+}
+
+function handleSpacebarKey(player, level) {
+  let closestB = wallUtil.closestWallBelow(player, level.arrWalls);
+  let distanceB = wallUtil.distanceBelow(player, closestB);
+  
+  if (distanceB > 2) {
+    player.jumping = true;
+    return
+  } else {
+    player.jumping = true;
+    player.jump();
   }
 }
