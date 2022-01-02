d2a9cbb02f1b8462a2a50031d7c5740141ab7e34
grayson-poon <grayson.a.poon@gmail.com>
2022-01-02 01:21:23 -0800

    refactor vec util file

--- src/scripts/Utils/vecUtil.js
+++ src/scripts/Utils/vecUtil.js
@@ -5,7 +5,7 @@ export const vecUtil = {
 
   normalize(playerPos, clickPos) {
     let vector = [clickPos[0] - playerPos[0], clickPos[1] - playerPos[1]];
-    let magnitude = vecUtil.vecMagnitude(playerPos, clickPos);
+    let magnitude = this.vecMagnitude(playerPos, clickPos);
 
     let unitX = (1 / magnitude) * vector[0];
     let unitY = (1 / magnitude) * vector[1];
