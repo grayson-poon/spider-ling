5f64af64060d8ee6c28bc8a2ed5d39da7c99933b
grayson-poon <grayson.a.poon@gmail.com>
2022-01-02 11:09:46 -0800

    refactor html classes, ids; refactor menubar listeners

--- src/scripts/event_handlers/menubar_listeners.js
+++ src/scripts/event_handlers/menubar_listeners.js
@@ -1,29 +1,32 @@
-const handleStart = (gameView, game) => {
-  const ele = document.getElementById("start_pause");
+const handleStartPause = (gameView, game) => {
+  const ele = document.getElementById("start-pause");
 
   ele.addEventListener("click", (event) => {
     event.stopPropagation();
     event.preventDefault();
 
-    if (ele.innerText === "Start") {
-      game.start(gameView);
-      ele.innerHTML = "Pause";
-      document.getElementById("canvas-container").style.border = "none";
-      document.getElementById(
-        "instructions_credits-container"
-      ).style.visibility = "hidden";
-    } else if (ele.innerText === "Pause") {
-      game.pause();
-      ele.innerHTML = "Resume";
-      document.getElementById("pause-container").style.visibility = "visible";
-    } else if (ele.innerText === "Resume") {
-      game.resume();
-      ele.innerHTML = "Pause";
-      document.getElementById("pause-container").style.visibility = "hidden";
-      document.getElementById(
-        "instructions_credits-container"
-      ).style.visibility = "hidden";
-      document.getElementById("canvas-container").style.border = "none";
+    switch(ele.innerText) {
+      case "Start":
+        ele.innerHTML = "Pause";
+        document.getElementById("canvas-container").style.border = "none";
+        document.getElementById("instructions-credits-container").style.visibility = "hidden";
+        
+        game.start(gameView);
+        break;
+      case "Pause":
+        ele.innerHTML = "Resume";
+        document.getElementById("pause-container").style.visibility = "visible";
+
+        game.pause();
+        break;
+      case "Resume":
+        ele.innerHTML = "Pause";
+        document.getElementById("pause-container").style.visibility = "hidden";
+        document.getElementById("instructions-credits-container").style.visibility = "hidden";
+        document.getElementById("canvas-container").style.border = "none";
+
+        game.resume();
+        break;
     }
   });
 };
@@ -32,41 +35,45 @@ const handleRestart = (gameView, game) => {
   const ele = document.getElementById("restart");
 
   ele.addEventListener("click", (event) => {
-    if (!game.gameStarted) return;
     event.stopPropagation();
     event.preventDefault();
-    document.getElementById("start_pause").innerText = "Pause";
+    if (!game.gameStarted) return;
+
+    document.getElementById("start-pause").innerText = "Pause";
     document.getElementById("pause-container").style.visibility = "hidden";
-    document.getElementById("instructions_credits-container").style.visibility = "hidden";
+    document.getElementById("instructions-credits-container").style.visibility = "hidden";
+
     game.restartLevel(gameView);
   });
 };
 
-const handleInstructions = (game) => {
-  const ele = document.getElementById("instructions_credits");
+const handleInstructionsCredits = (game) => {
+  const ele = document.getElementById("instructions-credits");
   
   ele.addEventListener("click", (event) => {
     if (!game.gameStarted) return;
     event.stopPropagation();
     event.preventDefault();
-    const ele = document.getElementById("instructions_credits-container");
+    const content = document.getElementById("instructions-credits-container");
 
-    if (ele.style.visibility === "visible") {
-      ele.style.visibility = "hidden";
-      game.resume();
-      document.getElementById("start_pause").innerHTML = "Pause";
+    if (content.style.visibility === "visible") {
+      content.style.visibility = "hidden";
+      document.getElementById("start-pause").innerHTML = "Pause";
       document.getElementById("canvas-container").style.border = "none";
+      
+      game.resume();
     } else {
-      ele.style.visibility = "visible";
-      game.pause();
-      document.getElementById("start_pause").innerHTML = "Resume";
+      content.style.visibility = "visible";
+      document.getElementById("start-pause").innerHTML = "Resume";
       document.getElementById("pause-container").style.visibility = "hidden";
+      
+      game.pause();
     }
   });
 };
 
 export const addMenubarListeners = (gameView, game) => {
-  handleStart(gameView, game);
+  handleStartPause(gameView, game);
   handleRestart(gameView, game);
-  handleInstructions(game);
+  handleInstructionsCredits(game);
 };
